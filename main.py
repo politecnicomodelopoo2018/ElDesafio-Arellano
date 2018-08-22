896f640b8976c86fcef7058a844632bbb303a550
jorge-arellan <arellano.ariel290@gmail.com>
2018-08-22 17:33:03 -0300

    signUp en proceso, hacer lo de la contraseña y el hasheo

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from classUsuario import *
 from datetime import date
 DB().setConnection('127.0.0.1', 'root', 'alumno', 'ElDesafio')
 
+
+
 app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
@@ -17,10 +19,22 @@ def Index():
 def PaginaPrincipal():
     return render_template("paginaPrincipal.html")
 
+@app.route('/signUp')
+def LogIn():
+    return render_template("signUp.html")
 
-usuario = Usuario.getUsuario(17)
-print(usuario.fechaCreacion)
+@app.route('/registrarUsuario', methods=['GET', 'POST'])
+def Registrar():
+    print("HOLAAA")
+    print(request.form.get("nombre"))
+    usuario = Usuario()
+    usuario.setNombre(request.form.get("nombre"))
+    usuario.setMail(request.form.get("mail"))
+    usuario.setFechaCreacion(date.today())
+    usuario.setNickName(request.form.get("nickName"))
+    usuario.setApellido(request.form.get("apellido"))
+    usuario.guardate()
+    return render_template("/paginaPrincipal.html")
 
-
-# if __name__ == '__main__':  # para actualizar automaticamente la pagina sin tener que cerrarla
-#     app.run(debug=True) # para correr la pagina se puede hacer en este caso "python3 PruebaFlask.py" en la terminal
+if __name__ == '__main__':  # para actualizar automaticamente la pagina sin tener que cerrarla
+    app.run(debug=True) # para correr la pagina se puede hacer en este caso "python3 PruebaFlask.py" en la terminal
